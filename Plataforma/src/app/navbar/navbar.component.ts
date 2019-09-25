4a6923ae9ccfc8482e8b54341feaea9b41dfa45f
BahamondeMatus <joaquinbah2016@udec.cl>
2019-09-25 09:48:22 -0300

    navbar actualizado

--- Plataforma/src/app/navbar/navbar.component.ts
+++ Plataforma/src/app/navbar/navbar.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent implements OnInit {
 
+  fireEvent(e){
+    console.log(e.type);
+    
+  }
   constructor() { }
 
   ngOnInit() {
+    $(".drop").mouseover(function() {
+        $(".dropdown").show(300);
+    });
+    $(".drop").mouseleave(function() {
+        $(".dropdown").hide(300);     
+    });
   }
+  
 
 }
